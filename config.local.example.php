f995be0607df2fe70f6ea1137d220b68ba1e98c5
danieldeploy <daniel.ciorcas@welcomehostel.pt>
2026-07-30 19:07:20 +0100

    Add local configuration example

---
+++ config.local.example.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'name' => 'welcome_roomcheck',
+        'user' => 'welcome_roomcheck',
+        'pass' => 'COLOQUE_A_PASSWORD_AQUI',
+    ],
+];
